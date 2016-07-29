Fixed the represent system to broadcast scalars in linear operators as scaled versions of the appropriate identity map if a shape option is passed to represent.

sympy/physics/quantum/pauli.py
@@ -108,23 +108,24 @@ class SigmaX(SigmaOpBase):
         size = options.get('size', 1)
         name = self.name
         if format == 'sympy':
-            eye = matrix_eye; kron = matrix_tensor_product
+            eye = matrix_eye; kron = matrix_tensor_product; wrap = lambda x: x
             M = Matrix([[0, 1], [1, 0]])
         elif format == 'numpy':
-            eye = np.eye; kron = np.kron
+            eye = np.eye; kron = np.kron; wrap = np.matrix
             M = np.array([[0, 1], [1, 0]])
         elif format == 'scipy.sparse':
             eye = sp.sparse.eye; kron = sp.sparse.kron
+            wrap = sp.sparse.aslinearoperator
             M = np.array([[0, 1], [1, 0]])
         else:
             raise NotImplementedError('Representation in format ' +
                                       format + ' not implemented.')
 
         if size is not None:
-            return kron(kron(eye(2**(self.name-1)), M),
-                        eye(2**(size - self.name)))
+            return wrap(kron(kron(eye(2**(self.name-1)), M),
+                             eye(2**(size - self.name))))
         else:
-            return M
+            return wrap(M)
 
 
 class SigmaY(SigmaOpBase):
@@ -193,23 +194,24 @@ class SigmaY(SigmaOpBase):
         size = options.get('size', 1)
         name = self.name
         if format == 'sympy':
-            eye = matrix_eye; kron = matrix_tensor_product
+            eye = matrix_eye; kron = matrix_tensor_product; wrap = lambda x: x
             M = Matrix([[0, -I], [I, 0]])
         elif format == 'numpy':
-            eye = np.eye; kron = np.kron
+            eye = np.eye; kron = np.kron; wrap = np.matrix
             M = np.array([[0, -1j], [1j, 0]])
         elif format == 'scipy.sparse':
             eye = sp.sparse.eye; kron = sp.sparse.kron
+            wrap = sp.sparse.aslinearoperator
             M = np.array([[0, -1j], [1j, 0]])
         else:
             raise NotImplementedError('Representation in format ' +
                                       format + ' not implemented.')
 
         if size is not None:
-            return kron(kron(eye(2**(self.name-1)), M),
-                        eye(2**(size - self.name)))
+            return wrap(kron(kron(eye(2**(self.name-1)), M),
+                                  eye(2**(size - self.name))))
         else:
-            return M
+            return wrap(M)
 
 class SigmaZ(SigmaOpBase):
     """Pauli sigma z operator
@@ -277,23 +279,24 @@ class SigmaZ(SigmaOpBase):
         size = options.get('size', 1)
         name = self.name
         if format == 'sympy':
-            eye = matrix_eye; kron = matrix_tensor_product
+            eye = matrix_eye; kron = matrix_tensor_product; wrap = lambda x: x
             M = Matrix([[1, 0], [0, -1]])
         elif format == 'numpy':
-            eye = np.eye; kron = np.kron
+            eye = np.eye; kron = np.kron; wrap = np.matrix
             M = np.array([[1, 0], [0, -1]])
         elif format == 'scipy.sparse':
             eye = sp.sparse.eye; kron = sp.sparse.kron
+            wrap = sp.sparse.aslinearoperator
             M = np.array([[1, 0], [0, -1]])
         else:
             raise NotImplementedError('Representation in format ' +
                                       format + ' not implemented.')
 
         if size is not None:
-            return kron(kron(eye(2**(self.name-1)), M),
-                        eye(2**(size - self.name)))
+            return wrap(kron(kron(eye(2**(self.name-1)), M),
+                             eye(2**(size - self.name))))
         else:
-            return M
+            return wrap(M)
 
 class SigmaMinus(SigmaOpBase):
     """Pauli sigma minus operator
@@ -375,23 +378,24 @@ class SigmaMinus(SigmaOpBase):
         size = options.get('size', 1)
         name = self.name
         if format == 'sympy':
-            eye = matrix_eye; kron = matrix_tensor_product
+            eye = matrix_eye; kron = matrix_tensor_product; wrap = lambda x: x
             M = Matrix([[0, 0], [1, 0]])
         elif format == 'numpy':
-            eye = np.eye; kron = np.kron
+            eye = np.eye; kron = np.kron; wrap = np.matrix
             M = np.array([[0, 0], [1, 0]])
         elif format == 'scipy.sparse':
             eye = sp.sparse.eye; kron = sp.sparse.kron
+            wrap = sp.sparse.aslinearoperator
             M = np.array([[0, 0], [1, 0]])
         else:
             raise NotImplementedError('Representation in format ' +
                                       format + ' not implemented.')
 
         if size is not None:
-            return kron(kron(eye(2**(self.name-1)), M),
-                        eye(2**(size - self.name)))
+            return wrap(kron(kron(eye(2**(self.name-1)), M),
+                             eye(2**(size - self.name))))
         else:
-            return M
+            return wrap(M)
 
 class SigmaPlus(SigmaOpBase):
     """Pauli sigma plus operator
@@ -479,23 +483,24 @@ class SigmaPlus(SigmaOpBase):
         size = options.get('size', 1)
         name = self.name
         if format == 'sympy':
-            eye = matrix_eye; kron = matrix_tensor_product
+            eye = matrix_eye; kron = matrix_tensor_product; wrap = lambda x: x
             M = Matrix([[0, 1], [0, 0]])
         elif format == 'numpy':
-            eye = np.eye; kron = np.kron
+            eye = np.eye; kron = np.kron; wrap = np.matrix
             M = np.array([[0, 1], [0, 0]])
         elif format == 'scipy.sparse':
             eye = sp.sparse.eye; kron = sp.sparse.kron
+            wrap = sp.sparse.aslinearoperator
             M = np.array([[0, 1], [0, 0]])
         else:
             raise NotImplementedError('Representation in format ' +
                                       format + ' not implemented.')
 
         if size is not None:
-            return kron(kron(eye(2**(self.name-1)), M),
-                        eye(2**(size - self.name)))
+            return wrap(kron(kron(eye(2**(self.name-1)), M),
+                             eye(2**(size - self.name))))
         else:
-            return M
+            return wrap(M)
 
 
 class SigmaZKet(Ket):

sympy/physics/quantum/represent.py
@@ -8,7 +8,7 @@ TODO:
 
 from __future__ import print_function, division
 
-from sympy import Add, Expr, I, integrate, Mul, Pow
+from sympy import Add, Expr, I, integrate, Mul, Pow, Number
 from sympy.physics.quantum.dagger import Dagger
 from sympy.physics.quantum.commutator import Commutator
 from sympy.physics.quantum.anticommutator import AntiCommutator
@@ -35,18 +35,34 @@ __all__ = [
 #-----------------------------------------------------------------------------
 
 
-def _sympy_to_scalar(e):
+def _sympy_to_scalar(e, format="numpy", size=1):
     """Convert from a sympy scalar to a Python scalar."""
-    if isinstance(e, Expr):
-        if e.is_Integer:
-            return int(e)
-        elif e.is_Float:
-            return float(e)
-        elif e.is_Rational:
-            return float(e)
-        elif e.is_Number or e.is_NumberSymbol or e == I:
-            return complex(e)
-    raise TypeError('Expected number, got: %r' % e)
+    import numpy as np
+    import scipy as sp
+    import scipy.sparse
+
+    if not isinstance(e, Expr):
+        raise TypeError('Expected number, got: %r' % e)
+    if e.is_Integer:
+        M = int(e)
+    elif e.is_Float:
+        M = float(e)
+    elif e.is_Rational:
+        M = float(e)
+    elif e.is_Number or e.is_NumberSymbol or e == I:
+        M = complex(e)
+    else:
+        raise TypeError('Expected number, got: %r' % e)
+
+    if size <= 1:
+        return M
+    else:
+        if format == 'numpy':
+            return np.matrix(M * np.eye(size))
+        elif format == 'scipy.sparse':
+            return np.aslinearoperator(M * sp.sparse.eye(size))
+        else:
+            raise ValueError('Unexpected format, got: %r' % e)
 
 
 def represent(expr, **options):
@@ -162,10 +178,17 @@ def represent(expr, **options):
             else:
                 raise NotImplementedError(strerr)
     elif isinstance(expr, Add):
-        result = represent(expr.args[0], **options)
+        if isinstance(expr.args[0], Number):
+            result = represent(expr.args[0], **dict(options, broadcast=True))
+        else:
+            result = represent(expr.args[0], **options)
         for args in expr.args[1:]:
             # scipy.sparse doesn't support += so we use plain = here.
-            result = result + represent(args, **options)
+            if isinstance(args, Number):
+                args = represent(args, **dict(options, broadcast=True))
+            else:
+                args = represent(args, **options)
+            result = result + args
         return result
     elif isinstance(expr, Pow):
         base, exp = expr.as_base_exp()
@@ -190,7 +213,11 @@ def represent(expr, **options):
     elif not (isinstance(expr, Mul) or isinstance(expr, OuterProduct)):
         # For numpy and scipy.sparse, we can only handle numerical prefactors.
         if format == 'numpy' or format == 'scipy.sparse':
-            return _sympy_to_scalar(expr)
+            if options.get('broadcast', False):
+                shape = options.get('shape', 1)
+            else:
+                shape = 1
+            return _sympy_to_scalar(expr, format, shape)
         return expr
 
     if not (isinstance(expr, Mul) or isinstance(expr, OuterProduct)):